4cd37e32c90ff090e0141026683cb7a2558b54bb
haochencheng <532224524@qq.com>
2018-08-28 17:29:04 +0800

    frist commit

---
+++ spring-cloud-eureka-client/src/main/java/pers/demo/springcloud/springcloudeurekaclient/SpringCloudEurekaClientApplication.java
@@ -0,0 +1,14 @@
+package pers.demo.springcloud.springcloudeurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SpringCloudEurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEurekaClientApplication.class, args);
+	}
+}
